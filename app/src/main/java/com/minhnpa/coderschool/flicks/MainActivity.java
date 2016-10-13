a75357807f818f44fae83c7fa50e3fdc8983601e
Minh Nguyen <minhnpa1907@gmail.com>
2016-10-13 23:55:55 +0700

    Second time commit

--- app/src/main/java/com/minhnpa/coderschool/flicks/MainActivity.java
+++ app/src/main/java/com/minhnpa/coderschool/flicks/MainActivity.java
@@ -2,12 +2,16 @@ package com.minhnpa.coderschool.flicks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvMovie = (ListView) findViewById(R.id.lvMovie);
     }
 }
