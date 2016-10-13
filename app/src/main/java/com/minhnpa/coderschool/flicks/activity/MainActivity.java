84d956c572f4f42942e05b9a9348714ec00c3aea
Minh Nguyen <minhnpa1907@gmail.com>
2016-10-14 00:31:29 +0700

    14 Oct 2016

--- app/src/main/java/com/minhnpa/coderschool/flicks/activity/MainActivity.java
+++ app/src/main/java/com/minhnpa/coderschool/flicks/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.minhnpa.coderschool.flicks;
+package com.minhnpa.coderschool.flicks.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.minhnpa.coderschool.flicks.R;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvMovie;
---
+++ app/src/main/java/com/minhnpa/coderschool/flicks/model/Movie.java
@@ -0,0 +1,37 @@
+package com.minhnpa.coderschool.flicks.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by MINH NPA on 14 Oct 2016.
+ */
+
+public class Movie {
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("overview")
+    private String overview;
+
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+}
