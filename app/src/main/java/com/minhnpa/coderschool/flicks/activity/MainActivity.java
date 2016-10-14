08be23bd1d943cb7caa7ed7d712a281913a1a2c4
Minh Nguyen <minhnpa1907@gmail.com>
2016-10-14 15:08:59 +0700

    15:08 - 14 Oct 2016

--- app/src/main/java/com/minhnpa/coderschool/flicks/activity/MainActivity.java
+++ app/src/main/java/com/minhnpa/coderschool/flicks/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.minhnpa.coderschool.flicks.activity;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar pbLoadMovie;
     private MovieApi mMovieApi;
 
+    // Handling Configuration
+    static final String SAVE_ORIENTATION = "save_orientation";
+    private int mSaveInt = 0;
+    private Response<NowPlaying> resp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,17 +38,29 @@ public class MainActivity extends AppCompatActivity {
         lvMovie = (ListView) findViewById(R.id.lvMovie);
         pbLoadMovie = (ProgressBar) findViewById(R.id.pbLoadMovie);
         mMovieApi = RetrofitUtils.get(getString(R.string.api_key)).create(MovieApi.class);
-        mMovieApi.getNowPlaying().enqueue(new Callback<NowPlaying>() {
-            @Override
-            public void onResponse(Call<NowPlaying> call, Response<NowPlaying> response) {
-                handleResponse(response);
-            }
+        if (mSaveInt == 0) {
+            mMovieApi.getNowPlaying().enqueue(new Callback<NowPlaying>() {
+                @Override
+                public void onResponse(Call<NowPlaying> call, Response<NowPlaying> response) {
+                    handleResponse(response);
+                    resp = response;
+                }
 
-            @Override
-            public void onFailure(Call<NowPlaying> call, Throwable t) {
-                Log.e("Error", t.getMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<NowPlaying> call, Throwable t) {
+                    Log.e("Error", t.getMessage());
+                }
+            });
+        } else {
+            handleResponse(resp);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        mSaveInt = 9001;
+        savedInstanceState.putInt(SAVE_ORIENTATION, mSaveInt);
     }
 
     private void handleResponse(Response<NowPlaying> response) {
